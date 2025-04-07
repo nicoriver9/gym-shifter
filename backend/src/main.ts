7e5e7caecd595f3e7f898e3d3796b1b36616652f
GabrielAlias <114373470+nicoriver9@users.noreply.github.com>
2025-04-07 17:37:25 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
