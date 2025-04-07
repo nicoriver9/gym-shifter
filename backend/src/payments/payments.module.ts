10a1db49376f0b1654cad5c562e42b6f5401d401
GabrielAlias <114373470+nicoriver9@users.noreply.github.com>
2025-04-07 16:54:33 -0300

    Update payments.module.ts

--- backend/src/payments/payments.module.ts
+++ backend/src/payments/payments.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { PrismaService } from '../prisma/prisma.service';
-import { UserPackService } from 'src/user-pack/user-pack.service';
+import { UserPackService } from '../user-pack/user-pack.service';
 
 
 @Module({
