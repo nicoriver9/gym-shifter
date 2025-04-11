f4e7de38473d29eb566b23cea007ac584a0c41e3
GabrielAlias <114373470+nicoriver9@users.noreply.github.com>
2025-04-11 16:46:23 -0300

    Update classes.service.ts

--- backend/src/classes/classes.service.ts
+++ backend/src/classes/classes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import * as moment from 'moment-timezone';
+import moment from 'moment-timezone';
 
 @Injectable()
 export class ClassesService {
